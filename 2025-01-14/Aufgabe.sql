592b197cc5fefe622ab6b93d97ac24392d26e3f9
Milan Pejcic <PEJ230110@spengergasse.at>
2025-01-20 22:18:12 +0100

    HÜ-Fertig

---
+++ 2025-01-14/Aufgabe.sql
@@ -0,0 +1,27 @@
+1.
+
+INSERT INTO persons (id, firstname, lastname, sex) VALUES (1, 'Henrik', 'Münch', 'male');
+
+INSERT INTO persons (id, firstname, lastname, sex) VALUES (2, 'Luzie', 'Kupprion', 'female');
+
+INSERT INTO persons (id, firstname, lastname, sex) VALUES (3, 'Kerim', 'Friedrich', 'male');
+
+2.
+
+INSERT INTO persons (id, firstname, lastname, sex)
+VALUES
+    (4, NULL, 'Hechler', NULL),
+    (5, 'Valeria', NULL, 'female'),
+    (6, NULL, NULL, NULL);
+
+3.
+
+SELECT * FROM persons WHERE sex IS NULL;
+
+SELECT * FROM persons WHERE firstname IS NULL OR lastname IS NULL;
+
+4.
+
+SELECT * FROM persons WHERE id IS NOT NULL; 
+
+SELECT * FROM persons WHERE sex IS NOT NULL;
