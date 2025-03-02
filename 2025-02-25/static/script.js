e14267fdd8c340137239bea8691ea466956ea63f
Milan Pejcic <PEJ230110@spengergasse.at>
2025-03-02 22:24:43 +0100

    Hü

--- 2025-02-25/static/script.js
+++ 2025-02-25/static/script.js
@@ -1,3 +1,9 @@
+const content = document.getElementsByTagName('body')[0];
+const darkMode = document.getElementById('dark-change');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+})
 for (i = 1; i <= 10; i++) {
     console.log(i);
     document.getElementById("liste").innerHTML+=` <li>${i}</li>`
