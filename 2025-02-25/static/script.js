62ca1ea9f6836daaec9afec49aa4ace2b10264ea
Milan Pejcic <PEJ230110@spengergasse.at>
2025-03-02 23:10:31 +0100

    Zwischenstand_SWP-HÜ

--- 2025-02-25/static/script.js
+++ 2025-02-25/static/script.js
@@ -4,7 +4,3 @@ darkMode.addEventListener('click', function(){
     darkMode.classList.toggle('active');
     content.classList.toggle('night');
 })
-for (i = 1; i <= 10; i++) {
-    console.log(i);
-    document.getElementById("liste").innerHTML+=` <li>${i}</li>`
-}
